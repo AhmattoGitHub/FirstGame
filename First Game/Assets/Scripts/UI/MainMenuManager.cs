869ce861bf17852b1d986c0e7b096a1469f5a473
AHMED\Ahmed <Ahmedyassine123@hotmail.com>
2024-03-05 06:48:57 -0500

    +Can now navigate between main and options menu +Can press play to launch the game and press quit to exit the application

---
+++ First Game/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_mainMenu;
+    [SerializeField]
+    private GameObject m_optionsMenu;
+
+    private static MainMenuManager _instance;
+
+    // Property to access the singleton instance
+    public static MainMenuManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("There is no Instance");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToOptionsMenu()
+    {
+        m_mainMenu.SetActive(false);
+        m_optionsMenu.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+        m_mainMenu.SetActive(true);
+        m_optionsMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
